8c2756019905df63ccd71e972e5f300b8c232ca7
ma3a <ivanoffi.ivan@yandex.ru>
2018-11-15 21:16:04 +0500

    fix status

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -15,20 +15,17 @@ class AuthorizeApiRequest
   private  
   
   def user
-    return errors.add( :token, 'Invalid token') if BlackList.exists?(token: http_auth_header)
+    # return errors.add( :token, 'Invalid token') if BlackList.exists?(token: http_auth_header)
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
     @user || errors.add(:token, 'Invalid token') && nil
   end
   
   def decoded_auth_token
-    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    token = http_auth_header
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header) if token
   end
   
   def http_auth_header
-    if headers['Authorization'].present?
-      return headers['Authorization'].split(' ').last
-    else 
-      errors.add(:token, 'Missing token')
-    end
+    return headers['Authorization'].split(' ').last if headers['Authorization'].present?
   end
 end
